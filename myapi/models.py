8994a6043d618a543986e52f2b792eac096525b5
Sheff2000 <sheffpb@gmail.com>
2024-03-18 13:31:40 +0200

    init migration

--- myapi/models.py
+++ myapi/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    dateCreate = models.DateTimeField(auto_now_add=True)
